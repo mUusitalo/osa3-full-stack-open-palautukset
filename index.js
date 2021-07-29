01ab4c7a2866d08628a5aa80cc9cb85f91bf1c98
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-07-29 14:34:42 +0300

    change environment variable port to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const db = {
     "persons": [
